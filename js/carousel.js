307c721b7e212eb7644e47275a720413578144a2
ivanTimoteo <iat1@discente.ifpe.edu.br>
2025-02-17 13:09:13 -0300

    Padrao footer no fim

--- js/carousel.js
+++ js/carousel.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const carousels = document.querySelectorAll(".produtos");
-    carousels.forEach(carousel => {
+    // Carrosséis automáticos
+    const carouselsAuto = document.querySelectorAll(".produtos-carrossel-auto");
+    
+    carouselsAuto.forEach(carousel => {
         let scrollAmount = 0;
         const step = 250;
         const maxScroll = carousel.scrollWidth - carousel.clientWidth;
-        
+
         function scrollCarousel() {
             if (scrollAmount < maxScroll) {
                 scrollAmount += step;
@@ -14,6 +16,26 @@ document.addEventListener("DOMContentLoaded", function () {
             carousel.scrollTo({ left: scrollAmount, behavior: "smooth" });
         }
         
-        setInterval(scrollCarousel, 5000);
+        setInterval(scrollCarousel, 4000); // O carrossel irá rolar automaticamente
+    });
+
+    // Carrosséis com botões de navegação
+    const carouselsManual = document.querySelectorAll(".produtos-carrossel-manual");
+    
+    carouselsManual.forEach(carousel => {
+        const scrollLeftButton = carousel.querySelector(".prev-btn");
+        const scrollRightButton = carousel.querySelector(".next-btn");
+
+        if (scrollLeftButton) {
+            scrollLeftButton.addEventListener("click", function() {
+                carousel.scrollBy({ left: -300, behavior: "smooth" }); // Move para a esquerda
+            });
+        }
+
+        if (scrollRightButton) {
+            scrollRightButton.addEventListener("click", function() {
+                carousel.scrollBy({ left: 300, behavior: "smooth" }); // Move para a direita
+            });
+        }
     });
 });
