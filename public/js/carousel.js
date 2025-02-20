7911741f82092b9150fe4d9bc54938128f91e3a8
IVANILSON ALVES TIMOTEO <20232ADSGR0047@labs.garanhuns.ifpe>
2025-02-20 20:31:34 -0300

    Adicionado todos os produtos do json para o front-end

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,10 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Carrosséis automáticos
     const carouselsAuto = document.querySelectorAll(".produtos-carrossel-auto");
-    
+
     carouselsAuto.forEach(carousel => {
         let scrollAmount = 0;
-        const step = 250;
+        const step = 500; // Ajuste o valor do step
         const maxScroll = carousel.scrollWidth - carousel.clientWidth;
 
         function scrollCarousel() {
@@ -13,29 +12,32 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 scrollAmount = 0;
             }
-            carousel.scrollTo({ left: scrollAmount, behavior: "smooth" });
-        }
-        
-        setInterval(scrollCarousel, 4000); // O carrossel irá rolar automaticamente
-    });
-
-    // Carrosséis com botões de navegação
-    const carouselsManual = document.querySelectorAll(".produtos-carrossel-manual");
-    
-    carouselsManual.forEach(carousel => {
-        const scrollLeftButton = carousel.querySelector(".prev-btn");
-        const scrollRightButton = carousel.querySelector(".next-btn");
-
-        if (scrollLeftButton) {
-            scrollLeftButton.addEventListener("click", function() {
-                carousel.scrollBy({ left: -300, behavior: "smooth" }); // Move para a esquerda
-            });
+            console.log('scrollAmount:', scrollAmount); // Debugging
+            console.log('maxScroll:', maxScroll); // Debugging
+            carousel.scrollLeft = scrollAmount;
         }
 
-        if (scrollRightButton) {
-            scrollRightButton.addEventListener("click", function() {
-                carousel.scrollBy({ left: 300, behavior: "smooth" }); // Move para a direita
-            });
-        }
+        setInterval(scrollCarousel, 9000);
     });
 });
+
+
+// // Carrosséis com botões de navegação
+// const carouselsManual = document.querySelectorAll(".produtos-carrossel-manual");
+
+// carouselsManual.forEach(carousel => {
+//     const scrollLeftButton = carousel.querySelector(".prev-btn");
+//     const scrollRightButton = carousel.querySelector(".next-btn");
+
+//     if (scrollLeftButton) {
+//         scrollLeftButton.addEventListener("click", function () {
+//             carousel.scrollBy({ left: -300, behavior: "smooth" }); // Move para a esquerda
+//         });
+//     }
+
+//     if (scrollRightButton) {
+//         scrollRightButton.addEventListener("click", function () {
+//             carousel.scrollBy({ left: 300, behavior: "smooth" }); // Move para a direita
+//         });
+//     }
+// });
--- public/js/produtos.js
+++ public/js/produtos.js
@@ -10,94 +10,94 @@ const estoque = [
 },
 {
     id: "produto-1",
-    img: "../images/produtos/",
+    img: "../images/produtos/minoxidil-low.jpg",
     nome: "Minoxidil",
     descricao: "Aproveite a promoção: leve 1, pague 2",
-    valor: 30.00
+    valor: 50.90
 },
 {
     id: "produto-2",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/cadeira-low.jpg",
+    nome: "Cadeira de Barbearia",
+    descricao: "Preço mais em conta para iniciantes.",
+    valor: 799.90
 },
 {
     id: "produto-3",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/tesoura.jpg",
+    nome: "Tesoura de Prata",
+    descricao: "A melhor tesoura do mercado.",
+    valor: 40.00
 },
 {
     id: "produto-4",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/tesoura-medium.png",
+    nome: "Tesoura Cromada",
+    descricao: "Um par de tesouras cromadas.",
+    valor: 59.90
 },
 {
     id: "produto-5",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/cadeira.jpg",
+    nome: "Cadeira Premium",
+    descricao: "Confortável e resistente, ideal para sua barbearia.",
+    valor: 1299.90
 },
 {
     id: "produto-6",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/espuma-low.jpg",
+    nome: "Espuma de Barbear",
+    descricao: "Espuma premium para um barbear suave.",
+    valor: 39.90
 },
 {
     id: "produto-7",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/minoxidil.jpg",
+    nome: "Minoxidil",
+    descricao: "Tratamento eficaz para o crescimento da sua barba.",
+    valor: 89.90
 },
 {
     id: "produto-8",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/tesoura-low.jpg",
+    nome: "Tesoura Simples",
+    descricao: "Tesoura simples, acabamento de plástico.",
+    valor: 20.00
 },
 {
     id: "produto-9",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/tintura1.jpg",
+    nome: "Tinta Capilar",
+    descricao: "Tintura branca, nevou ai?",
+    valor: 38.90
 },
 {
     id: "produto-10",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/tintura2.jpg",
+    nome: "Tinta Capilar Premium",
+    descricao: "Tinta castanho especial para cabelos crespos.",
+    valor: 50.90
 },
 {
     id: "produto-11",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/tintura3.jpg",
+    nome: "Tintura Clássica",
+    descricao: "Tintura clássica castanho escuro",
+    valor: 30.90
 },
 {
     id: "produto-12",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/tintura4.jpg",
+    nome: "Tintura Clássica Biocolor",
+    descricao: "Excelente tintura para o seu cabelo",
+    valor: 40.00
 },
 {
     id: "produto-13",
-    img: "../images/produtos/",
-    nome: "",
-    descricao: "",
-    valor: 0.0
+    img: "../images/produtos/tintura5.jpg",
+    nome: "Tintura Econômica Cor&Ton",
+    descricao: "Sachê economica castanho escura.",
+    valor: 29.90
 }
 ]
 
