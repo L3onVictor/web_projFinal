4da75c6cf6ca2d429ad27df39b5f6c6ce7d771e6
ivanTimoteo <ivanilsonat@outlook.com>
2025-02-20 16:24:48 -0300

    json dos produtos
    inicio do desenvolvimento dos produtos no arquivo json

---
+++ public/js/produtos.js
@@ -0,0 +1,105 @@
+window.addEventListener("load", main)
+
+function main() {
+
+    const produtos = [
+    {   
+        id: "produto-0",
+        img: "../images/produtos/espuma.jpg",
+        nome: "Espuma de Barbear",
+        descricao: "Promoção de espuma de barbear.",
+        preco: 18.90
+    },
+    {
+        id: "produto-1",
+        img: "../images/produtos/",
+        nome: "Minoxidil",
+        descricao: "Aproveite a promoção: leve 1, pague 2",
+        preco: 30.00
+    },
+    {
+        id: "produto-2",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-3",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-4",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-5",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-6",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-7",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-8",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-9",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-10",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-11",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-12",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    },
+    {
+        id: "produto-13",
+        img: "../images/produtos/",
+        nome: "",
+        descricao: "",
+        preco: 0.0
+    }
+]
+}
